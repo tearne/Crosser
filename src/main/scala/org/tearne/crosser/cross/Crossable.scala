76674f93a86805cf4832187b90c3728f09e003e6
Oliver Tearne <user@server>
2012-07-14 21:29:27 +0100

    Crossable should include sample method

--- src/main/scala/org/tearne/crosser/cross/Crossable.scala
+++ src/main/scala/org/tearne/crosser/cross/Crossable.scala
@@ -1,7 +1,13 @@
 package org.tearne.crosser.cross
 
-trait Crossable
+import org.tearne.crosser.distribution.ChromosomeBank
+import org.tearne.crosser.util.Random
+import org.tearne.crosser.plant.ConcretePlant
+
+trait Crossable{
+	def sample(cBank: ChromosomeBank, rnd: Random): ConcretePlant
+}
 
 case class Cross(left: Crossable, right: Crossable, protocol: Protocol, name: String) extends Crossable{
-	
+	def sample(cBank: ChromosomeBank, rnd: Random): ConcretePlant = throw new UnsupportedOperationException("TODO")
 }
--- src/main/scala/org/tearne/crosser/plant/ConcretePlants.scala
+++ src/main/scala/org/tearne/crosser/plant/ConcretePlants.scala
@@ -2,6 +2,8 @@ package org.tearne.crosser.plant
 
 import org.tearne.crosser.cross.Crossable
 import org.tearne.crosser.plant.exception.PlantException
+import org.tearne.crosser.distribution.ChromosomeBank
+import org.tearne.crosser.util.Random
 
 sealed trait ConcretePlant extends Crossable{
 	val chromosomes: IndexedSeq[Chromosome]
@@ -10,6 +12,8 @@ sealed trait ConcretePlant extends Crossable{
 case class RootPlant(val name: String, val species: Species) extends ConcretePlant{
 	val chromosomes = species.buildChromosomesFrom(this)
 
+	def sample(cBank: ChromosomeBank, rnd: Random): ConcretePlant = throw new UnsupportedOperationException("TODO")
+	
 	override def toString() = name
 	def canEqual(other: Any) = other.isInstanceOf[RootPlant]
 	override def equals(other: Any) = {
@@ -23,6 +27,9 @@ case class RootPlant(val name: String, val species: Species) extends ConcretePla
 }
 
 case class Plant(val name: String, val chromosomes: IndexedSeq[Chromosome], val species: Species) extends ConcretePlant {
+	
+	def sample(cBank: ChromosomeBank, rnd: Random): ConcretePlant = throw new UnsupportedOperationException("TODO")
+	
 	if(species.cMLengths != chromosomes.map(_.size))
 		throw new PlantException("Chromosomes for plant %s do not match species (%s)".format(name, species))
 }
